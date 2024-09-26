9bf96a9d2d336dc7248b85f68cf0e3b912c40b09
junerockwell <devapprocks@gmail.com>
2024-09-26 11:49:37 -0500

    Updated for demo.

--- cypress/e2e/modalForm.spec.ts
+++ cypress/e2e/modalForm.spec.ts
@@ -175,17 +175,7 @@ describe("Modal Form", () => {
     cy.getBySel("modal-with-form").should("be.visible");
     cy.getBySel("radio-choices-field")
       .find("input[type='radio']")
-      .check("blueberry")
-      .should("be.checked");
-    cy.wait(1000);
-    cy.getBySel("radio-choices-field")
-      .find("input[type='radio']")
       .check("regular")
-      .should("be.checked");
-    cy.wait(1000);
-    cy.getBySel("radio-choices-field")
-      .find("input[type='radio']")
-      .check("lime")
       .should("be.checked");
     cy.wait(1000);
   });
